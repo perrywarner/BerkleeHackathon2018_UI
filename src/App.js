4b7fbac634d9a1278b334031c311b26056cd69de
Perry Warner <perrywarner11@gmail.com>
2018-10-06 01:51:37 -0400

    Add minimal Bootstrap integration to project. Implement initial button.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -9,8 +10,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. TestTest
           </p>
+          <Button/>
           <a
             className="App-link"
             href="https://reactjs.org"
