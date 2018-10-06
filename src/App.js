0af7de8736bcf7a1bea676a59aeceaf949b7b64c
Perry Warner <perrywarner11@gmail.com>
2018-10-06 16:09:57 -0400

    Document App.js with next steps, add function prototypes for eventLoop() and sendStateToController() to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ButtonToggle from './components/buttontoggle/index.js';
-import Moment from 'moment';
+import moment from 'moment';
 
 class App extends Component {
 
@@ -13,27 +13,41 @@ class App extends Component {
     this.handleButtonToggleChanged = this.handleButtonToggleChanged.bind(this);
 
     this.state = {
+      timeStamp : moment.format(),
       slider14 : DEFAULT_VALUE,
       slider15 : DEFAULT_VALUE,
       slider16 : DEFAULT_VALUE,
       slider17 : DEFAULT_VALUE,
       buttonToggle18 : DEFAULT_VALUE,
-      buttonMomentary19 : DEFAULT_VALUE,
+      buttonMomentary19 : DEFAULT_VALUE
     }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    // call eventLoop() here
+  }
+
+  eventLoop() {
+    // timer that calls sendStateToController() every 100ms
+    // use moment.js (https://momentjs.com/guides/)
+    // likely important: reference http://momentjs.com/docs/#/parsing/unix-timestamp-milliseconds/ millisecond time differential
+  }
+
+  sendStateToController() {
+    // use Fetch API to post JSON object of each of this application's state values 
+    // to our backend here. for reference: (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) 
+  }
 
   handleButtonToggleChanged(id, value) {
-    
+    // apply state changes to the component that corresponds with the ID specified in the state value list (this.state{})
   }
 
   handleButtonMomentaryChanged(id, value) {
-    
+    // apply state changes to the component that corresponds with the ID specified in the state value list (this.state{})
   }
 
   handleSliderChanged(id, value) {
-    
+    // apply state changes to the component that corresponds with the ID specified in the state value list (this.state{})
   }
 
   render() {
